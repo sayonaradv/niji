24b2743926c64da8362da23b9d1a841b11cc6b2c
shinigami-dv <107803920+shinigami-dv@users.noreply.github.com>
2025-08-22 18:41:38 +0100

    fix: add type annotations and remove unused utils

--- src/blanket/predictor.py
+++ src/blanket/predictor.py
@@ -1,6 +1,8 @@
 import os
 
 import torch
+from lightning.pytorch import LightningModule
+from torch import Tensor
 
 from blanket.models import ToxicityClassifier
 
@@ -9,7 +11,7 @@ os.environ["TOKENIZERS_PARALLELISM"] = "false"
 
 class Blanket:
     def __init__(self, checkpoint: str, threshold: float = 0.5, device="cpu") -> None:
-        self.model = ToxicityClassifier.load_from_checkpoint(
+        self.model: LightningModule = ToxicityClassifier.load_from_checkpoint(
             checkpoint, map_location=device
         )
 
@@ -17,7 +19,7 @@ class Blanket:
     def predict(self, text: str | list[str]) -> None:
         self.model.eval()
         text = [text] if isinstance(text, str) else text
-        predictions = self.model(text)["outputs"].detach().cpu()
+        predictions: Tensor = self.model(text)["outputs"].detach().cpu()
 
         for _text, _pred in zip(text, predictions, strict=True):
             print(f"\nText: {_text}")
--- src/blanket/utils.py
+++ src/blanket/utils.py
@@ -1,7 +1,3 @@
-from pathlib import Path
-from typing import Any
-
-from torch import Tensor
 from transformers import (
     AutoModelForSequenceClassification,
     AutoTokenizer,
@@ -24,31 +20,3 @@ def get_model_and_tokenizer(
         model_name, cache_dir=cache_dir, use_fast=True
     )
     return model, tokenizer
-
-
-def move_to(obj: Any, device: str) -> Any:
-    if isinstance(obj, Tensor):
-        return obj.to(device=device)
-
-    if isinstance(obj, dict):
-        res = {}
-        for k, v in obj.items():
-            res[k] = move_to(v, device)
-        return res
-
-    if isinstance(obj, list):
-        res = []
-        for v in obj:
-            res.append(move_to(v, device))
-        return res
-
-    msg = f"Invalid type for device movement: {type(obj)}. Supported types: Tensor, dict, list"
-    raise TypeError(msg)
-
-
-def create_dirs(dirs: list[str | Path]) -> None:
-    for d in dirs:
-        path = Path(d) if isinstance(d, str) else d
-
-        if not path.is_dir():
-            path.mkdir(parents=True, exist_ok=True)
