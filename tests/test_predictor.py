4d9cc44f94a56316d292f229261508e16587a130
zuzo-sh <107803920+zuzo-sh@users.noreply.github.com>
2025-08-31 15:39:14 +0100

    feat: build out predictor tests

--- tests/test_predictor.py
+++ tests/test_predictor.py
@@ -14,3 +14,7 @@ class TestRuffle:
 
         with pytest.raises(ValueError, match="Threshold must be between 0.0 and 1.0"):
             Ruffle(threshold=-0.1)
+
+    def test_init_with_invalid_ckpt_path(self):
+        with pytest.raises(FileNotFoundError, match="No such file or directory"):
+            Ruffle(checkpoint_path="invalid_path")
