95090c602593d45bd85234d28c0d26f1cc810eb6
jayder <107803920+sudojayder@users.noreply.github.com>
2025-09-30 17:00:16 +0100

    feat: refactor __init__.py to import exceptions and setup functions directly, enhancing module structure

--- src/blanki/__init__.py
+++ src/blanki/__init__.py
@@ -1,9 +1,17 @@
-from blanki import setup
+from .exceptions import BlankiError, DataNotFoundError, ModelNotFoundError
+from .setup import env_vars, logging
 
-setup.env_vars()
-setup.logging()
+env_vars()
+logging()
+
 
 __version__ = "0.0.1a4"
+
+__all__: list[str] = [
+    "BlankiError",
+    "DataNotFoundError",
+    "ModelNotFoundError",
+]
 
 
 def main() -> None:
